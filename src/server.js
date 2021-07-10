081a9b4011e2131025749186bfb209988d61535c
GerA <lpgera@gmail.com>
2021-07-10 16:47:41 +0200

    implement base of backend server

--- src/server.js
+++ src/server.js
@@ -1,22 +1,44 @@
 require('dotenv').config()
+
 const express = require('express')
+const util = require('util')
+const childProcess = require('child_process')
+
+const exec = util.promisify(childProcess.exec)
 const app = express()
 const port = process.env.PORT ?? 3000
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+const executeIrCommand = async (command) => {
+  try {
+    await exec(`irsend SEND_ONCE ac ${command}`)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+app.use('/', express.static('public'))
+
+app.post('/cool', async (request, response) => {
+  await executeIrCommand('cool')
+  response.status(204)
+  response.send()
 })
 
-app.post('/cool', () => {
-
+app.post('/fan', async (request, response) => {
+  await executeIrCommand('fan')
+  response.status(204)
+  response.send()
 })
 
-app.post('/fan', () => {
-
+app.post('/off', async (request, response) => {
+  await executeIrCommand('off')
+  response.status(204)
+  response.send()
 })
 
-app.post('/off', () => {
-
+app.use((request, response) => {
+  response.status(404)
+  response.send('Not found')
 })
 
 app.listen(port, () => {
